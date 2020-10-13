4725b9ce59e9803eec5f6d0f8bdd82227b8898a2
Abdullah-MGK <abdullah_mgk@outlook.com>
2020-10-13 11:13:41 +0300

    implement create_artist_submission()

--- app.py
+++ app.py
@@ -225,6 +225,8 @@ def create_venue_form():
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
+  # called upon submitting the new venue listing form
+
   try:
     # TODO: insert form data as a new Venue record in the db, instead
     # TODO: modify data to be the data object returned from db insertion
@@ -461,13 +463,57 @@ def create_artist_form():
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
   # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
 
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+  try:
+    # TODO: insert form data as a new Venue record in the db, instead
+    # TODO: modify data to be the data object returned from db insertion
+    print("try", file = sys.stderr)
+    form = ArtistForm()
+
+    # MY TODO: if not form.validate_on_submit():
+      #print("invalid form", file = sys.stderr)
+      #raise Exception("invalid data")
+
+    # MY TODO: add other attributes
+    new_artist = Artist(
+      name = form.name.data,
+      city = form.city.data,
+      state = form.state.data,
+      phone = form.phone.data,
+      genres = form.genres.data,
+      image_link = form.image_link.data,
+      facebook_link = form.facebook_link.data,
+    )
+
+    # MY TODO: Another way
+    '''
+    new_venue = Venue(
+        name = request.form['name'],
+        city = request.form['city'],
+        state = request.form['state'],
+        phone = request.form['phone'],
+        genres = request.form['genres'],
+        image_link=request.form['image_link'],
+        facebook_link = request.form['facebook_link']
+    )
+    '''
+
+    db.session.add(new_artist)
+    db.session.commit()
+
+    # on successful db insert, flash success
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
+
+  except:
+  # [DONE] TODO: on unsuccessful db insert, flash an error instead.
+    print("except", file = sys.stderr)
+    db.session.rollback()
+    flash('An error occurred. Artist ' + request.form['name'] + ' could not be listed.')
+
+  finally:
+    print("finally", file = sys.stderr)
+    db.session.close()
+
   return render_template('pages/home.html')
 
 
