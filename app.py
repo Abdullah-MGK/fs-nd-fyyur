a613d40c3e8438dfcaab9a62a9ff7059672002ca
Abdullah-MGK <abdullah_mgk@outlook.com>
2020-10-14 02:57:40 +0300

    add missing fields to all models

--- app.py
+++ app.py
@@ -38,13 +38,18 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    #genres = db.Column(db.ARRAY(db.String))
+    genres = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    website = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     shows = db.relationship('Show', backref='venue', lazy=True)
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
     
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -53,13 +58,17 @@ class Artist(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    #genres = db.Column(db.ARRAY(db.String))
+    genres = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    website = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     shows = db.relationship('Show', backref='artist', lazy=True)
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -71,7 +80,7 @@ class Show(db.Model):
     start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-
+    
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 
